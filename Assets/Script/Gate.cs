4b273fa393a0c3e159878c6c20bfa12d763fc21d
Keijiro-cgp <Keijiro.cgp@gmail.com>
2019-09-25 14:37:27 +0900

    ゲートのアニメーション修正
    一つ分しかできてないです
    開閉が２回されてしまうバグがあります

--- Assets/Script/Gate.cs
+++ Assets/Script/Gate.cs
@@ -8,7 +8,6 @@ public class Gate : MonoBehaviour
 
     private GameObject Player;
     private Animator animator;
-    [SerializeField] private Animation anim;
     private bool through = false;
     [SerializeField]
     int gate_num = 0;
@@ -18,7 +17,6 @@ public class Gate : MonoBehaviour
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
-        anim = gameObject.GetComponent<Animation>();
         animator = gameObject.GetComponent<Animator>();
         checkPoint = Player.GetComponent<CheckPoint>();
     }
@@ -27,29 +25,20 @@ public class Gate : MonoBehaviour
     void FixedUpdate()
     {
         //Debug.Log(animator.GetCurrentAnimatorStateInfo(1).normalizedTime);
-        if(Vector3.Distance(Player.transform.position, transform.position) < 2.0f && !through) //プレイヤーとゲートの距離が2,0以下のとき
+        if(Vector3.Distance(Player.transform.position, transform.position) < 2.0f) //プレイヤーとゲートの距離が2,0以下のとき
         {
-            animator.Play("Armature|ArmatureAction"); //アニメーションの再生
-            if (animator.GetCurrentAnimatorStateInfo(1).normalizedTime > 0.5f)
-            {
-                
-                animator.speed = 0.0f;
-            }
+            animator.SetTrigger("Open_Trigger"); //開くアニメーションの再生
         }
         else
         {
-            if (animator.GetCurrentAnimatorStateInfo(1).normalizedTime >  0.5f)
-            {
-                animator.speed = 1.0f;
-                
-            }
-            
+            animator.SetTrigger("Close_Trigger"); //閉じるアニメーションの再生
         }
 
         if(Vector3.Distance(Player.transform.position, transform.position) < 0.5f && !through) //プレイヤーとゲートの距離が0.5以下でかつ、一度もそのゲートを通っていないとき
         {
             through = true; //すでにゲートを通ったことにする
             checkPoint.CPSave(gate_num); //CPSaveにゲート番号を渡す
+            
         }
     }
 }
